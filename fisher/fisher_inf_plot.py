443626580264994fb64e66053a1dac8b4fb6f4df
Krzysztof Skrzypczak <kskrzypczak9@gmail.com>
2020-10-13 16:41:49 +0100

    Changing scripts into class modules
    Taylor expansion code now allows for any function to be analysed
    bloch_anim now has a class for animating a pre-calculated states

--- fisher/fisher_inf_plot.py
+++ fisher/fisher_inf_plot.py
@@ -4,76 +4,54 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from mpl_toolkits.mplot3d import Axes3D
 
-t = np.linspace(1, 50,500)*(10**(-3))
-T1 = np.linspace(1, 10, 500)*(10**(-3))
+def fisher_inf(t, T1):
 
-#X is t, and Y is T1
+    X, Y = np.meshgrid(t, T1)
 
-X, Y = np.meshgrid(t, T1)
-
-def ticks(lists, number):
-    n = len(lists)
-    skip = int(n/number)
-    arr = [None]*number
-    ind = 0
-    for i in range(number):
-        arr[i] = lists[ind]
-        ind += skip
-    return arr
+    def ticks(lists, number):
+        n = len(lists)
+        skip = int(n/number)
+        arr = [None]*number
+        ind = 0
+        for i in range(number):
+            arr[i] = lists[ind]
+            ind += skip
+        return arr
 
 
-I = (X**2)/((Y**4)*(np.e**(X/Y)-1))
+    I = (X**2)/((Y**4)*(np.e**(X/Y)-1))
 
-I = np.log10(I)
+    I2 = np.log10(I)
 
-fig, ax = plt.subplots(1,1)
+    fig, ax = plt.subplots(1,2)
 
-img = ax.imshow(I)
+    img = ax[0].imshow(I)
 
-ax.set_xticks(list(np.linspace(0,len(t),10)))
-ax.set_xticklabels(np.round(ticks(t*1000, 10), 2))
-ax.set_yticks(list(np.linspace(0,len(T1),10)))
-ax.set_yticklabels(np.round(ticks(T1*1000, 10), 2))
+    img = ax[1].imshow(I2)
 
-plt.show()
+    ax[0].set_xticks(list(np.linspace(0,len(t),10)))
+    ax[0].set_xticklabels(np.round(ticks(t*1000, 10), 2), rotation = 45)
+    ax[0].set_yticks(list(np.linspace(0,len(T1),10)))
+    ax[0].set_yticklabels(np.round(ticks(T1*1000, 10), 2))
+    ax[0].set_title('Fisher Information')
+    ax[0].set_ylabel('T1')
+    ax[0].set_xlabel('t')
 
+    ax[1].set_xticks(list(np.linspace(0,len(t),10)))
+    ax[1].set_xticklabels(np.round(ticks(t*1000, 10), 2), rotation = 45)
+    ax[1].set_yticks(list(np.linspace(0,len(T1),10)))
+    ax[1].set_yticklabels(np.round(ticks(T1*1000, 10), 2))
+    ax[1].set_title('Fisher Information \nin a $log_{10}$ scale')
+    ax[1].set_ylabel('T1')
+    ax[1].set_xlabel('t')
 
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
+    fig.colorbar(img, ax = ax.ravel().tolist())
+    # fig.tight_layout()
 
-# surf = ax.plot_surface(X, Y, I, cmap='plasma', linewidth=0, antialiased=False)
-# ax.set_xticklabels(np.round(t*1000,2))
-# ax.set_yticklabels(np.round(T1*1000,2))
-# ax.set_xlabel('$T1*10^{-6}$')
-# ax.set_ylabel('$t*10^{-6}$')
-# ax.set_title('Fisher Information in log$_{10}$ scale')
-# fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
 
-# plt.tight_layout()
-# plt.show()
+if __name__ == '__main__':
+    t = np.linspace(1, 50,500)*(10**(-3))
+    T1 = np.linspace(1, 10, 500)*(10**(-3))
 
-# plt.imshow(I)
-# plt.colorbar(shrink=0.5, aspect=5)
-# # plt.xticklabels(np.round(t*1000,2))
-# # plt.yticklabels(np.round(T1*1000,2))
-# plt.xlabel('$T1*10^{-3}$')
-# plt.ylabel('$t*10^{-3}$')
-# plt.title('Fisher Information in log$_{10}$ scale')
-# plt.show()
-
-# # plt.hist2d(t,T1)#,[len(t), len(T1)])
-
-# # ax.set_yticklabels(vegetables)
-
-# plt.show()
-
-# # fig = plt.figure()
-# # ax = fig.gca(projection='3d')
-
-# # surf = ax.plot_surface(Y, X, I, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-# # ax.set_xlabel('T1')
-# # ax.set_ylabel('t')
-# # fig.colorbar(surf, shrink=0.5, aspect=5)
-
-# # plt.tight_layout()
-# # plt.show()
+    fisher_inf(t, T1)
--- fisher/taylor.py
+++ fisher/taylor.py
@@ -3,95 +3,140 @@ import numpy as np
 from sympy import *
 import time as tm
 
-init_printing(use_unicode=True)
 
 
-# Taylor::
-# f ~~ f(a) + f'(a)(x-a) + f''(a)/2 * (x-a)^2
+class taylor_exp:
+
+    """
+    A class that calculates a Taylor expansion of a given function
+
+    Attributes:
+        fun: function (symppy function fype)
+        order: order of the approsimation
+        around: around which point to evaluate the function
+        legend_lim: set a limit to which order to draw the legend (useful when making animations)
+        variable: function variable (usually 'x') as a sympy variable type
+        xrange: range of x values to evaluate the function at. list or numpy.array type
+
+    Methods:
+        plot_func: plots a function along with it's Taylor expansion plots
+        anim_func: makes an animated plot of a function along with its Taylor expansion
+
+    """
+    def __init__(self, fun, order, around, variable, xrange, legend_lim = False):
+        self.fun = fun
+        self.order = order
+        self.around = around
+        self.legend_lim = legend_lim
+        self.variable = variable
+        self.xrange = xrange
+        init_printing(use_unicode=True)
 
 
-def factorial(n):
-    if n < 2:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-def taylor(func, variable, a, order):
-    f1 = func.subs(variable, a)
-    f_der = {}
-    for i in range(1, order+1):
-        f_der[f"order{i}"] = (1/(factorial(i))) * ((diff(func, variable, i)).subs(variable, a))*((variable - a)**i)
-
-    result = 0
-    # if order > 1:
-    for i in list(f_der.values()):
-        result += i
-
-    return f1 + result
-
-
-if __name__ == '__main__':
-    x = symbols('x')
-
-    f = x**3
-
-    nums = np.linspace(-1*np.pi,1*np.pi,50)
-    lam_f = lambdify(x, f, 'numpy')
-    around = 1
-
-    leg = [f]
-    plt.ion()
-    fig, ax = plt.subplots(1,1)
-    # fig.show()
-
-
-    ax.set_title("Taylor expansion of {} around {}".format(f, around))
-
-
-    ax.plot(nums, lam_f(nums),'x')#, 'x', animated = True)
-
-    ax.set_ylim([-5, 20])
-    ax.set_xlim([min(nums), max(nums)])
-
-    plt.pause(0.5)
-    plt.block = True
-
-    ord = 3
-    ind2 = 0
-
-
-
-
-    for i in range(1, ord + 1):
-        
-
-        f_taylor = taylor(f, x, around, i)
-        y = [None]*len(nums)
-        ind = 0
-        for i in nums:
-            y[ind] = f_taylor.subs(x, i)
-            ind +=1
-
-        ax.plot(nums, y)#, animated = True)
-
-        ind2 += 1
-        n = "order {}".format(ind2)
-        if ind2<=4:
-            leg.append(n)
-            ax.legend(leg)
-
+    def factorial(self, n):
+        if n < 2:
+            return 1
         else:
-            leg = []
+            return n * factorial(n-1)
+    
+    def eval(self):
+        lam_f = lambdify(self.variable, self.fun, 'numpy')
+
+        leg = [self.fun]
+        fig, ax = plt.subplots(1,1)
+        ind2 = 0
+        for i in range(1, self.order + 1):
+            f_taylor = self.taylor(i)
+            y = [None]*len(self.xrange)
+            ind = 0
+            for i in self.xrange:
+                y[ind] = f_taylor.subs(self.variable, i)
+                ind +=1
+            ind2 += 1
+
+
+    def taylor(self):
+        f1 = self.fun.subs(self.variable, self.around)
+        f_der = {}
+        for i in range(1, self.order+1):
+            f_der[f"order{i}"] = (1/(self.factorial(i))) * ((diff(self.fun, self.variable, i)).subs(self.variable, self.around))*((self.variable - self.around)**i)
+
+        result = 0
+        for i in list(f_der.values()):
+            result += i
+        return f1 + result
+
+    def plot_func(self):
+        lam_f = lambdify(self.variable, self.fun, 'numpy')
+
+        leg = [self.fun]
+        fig, ax = plt.subplots(1,1)
+        ax.set_title("Taylor expansion of {} around {}".format(self.fun, self.around))
+        ax.plot(self.xrange, lam_f(self.xrange),'x')#, 'x', animated = True)
+        ind2 = 0
+        for i in range(1, self.order + 1):
+            self.order = i
+            f_taylor = self.taylor()
+            y = [None]*len(self.xrange)
+            ind = 0
+            for i in self.xrange:
+                y[ind] = f_taylor.subs(self.variable, i)
+                ind +=1
+            ax.plot(self.xrange, y)
+            ind2 += 1
+            n = "order {}".format(ind2)
+            if self.legend_lim == False:
+                leg.append(n)
+
+            else:
+                if ind2<=self.legend_lim:
+                    leg.append(n)
+                    ax.legend(leg)
+                else:
+                    pass
+            
+            ax.legend(leg)    
+            ax.set_title("Taylor expansion of {} around {}".format(self.fun, self.around))                
+        fig.show()
+    
+    def anim_func(self):
+
+        lam_f = lambdify(self.variable, self.fun, 'numpy')
+
+        leg = [self.fun]
+        plt.ion()
+        fig, ax = plt.subplots(1,1)
+        ax.set_title("Taylor expansion of {} around {}".format(self.fun, self.around))
+        ax.plot(self.xrange, lam_f(self.xrange),'x')#, 'x', animated = True)
+
         plt.pause(0.5)
+        plt.block = True
 
+        ind2 = 0
 
-        
-        # fig.draw()
+        for i in range(1, self.order + 1):
+            self.order = i
+            f_taylor = self.taylor()
+            y = [None]*len(self.xrange)
+            ind = 0
+            for i in self.xrange:
+                y[ind] = f_taylor.subs(self.variable, i)
+                ind +=1
 
-    plt.ioff()
+            ax.plot(self.xrange, y)
 
-    ax.legend(leg)    
-    ax.set_title("Taylor expansion of {} around {}".format(f, around))
+            ind2 += 1
+            n = "order {}".format(ind2)
+            if self.legend_lim == False:
+                leg.append(n)
 
-    input('')
-# fig.show()
+            else:
+                if ind2<=self.legend_lim:
+                    leg.append(n)
+                    ax.legend(leg)
+                else:
+                    pass
+            
+            ax.legend(leg)    
+            ax.set_title("Taylor expansion of {} around {}".format(self.fun, self.around))                
+            plt.pause(1)
